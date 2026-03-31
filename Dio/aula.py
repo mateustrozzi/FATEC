b209f61e7175df19eff89c2f6dd1884bb40f340d
mateustrozzi <mateustrozzi@outlook.com>
2026-03-31 16:11:10 -0300

    Adicionando arquivos de cursos e da apresentação

---
+++ Dio/aula.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+print("Hello World!")
+print(10 + 1)
+print(10 - 0.5)
+print(True)
+print("Isso é uma cadeia de caracteres - a popular string!")
+print( "texto" + """1""")
+print("Use python -i nome_script.py para usar o modo interativo direto do seu script")
+print("Use dir() ou dir(objeto) para ver os métodos\nOu help() para a ajuda interativa.")
+print("Para mais visite: https://wiki.python.org.br/ModoInterativo")
