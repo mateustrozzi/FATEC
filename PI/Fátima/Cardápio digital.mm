67c62aacdd45e29d7ca291b28934967c70b96c62
mateustrozzi <mateustrozzi@outlook.com>
2026-04-01 13:49:38 -0300

    Adicionando pasta Projeto Integrador e mapa mental

---
+++ PI/Fátima/Cardápio digital.mm
@@ -0,0 +1,144 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="ID_1845149238" name="Home" opensAsRoot="false"/>
+    <bookmark nodeId="ID_974004934" name="Contato" opensAsRoot="false"/>
+    <bookmark nodeId="ID_394082670" name="Sobre" opensAsRoot="false"/>
+    <bookmark nodeId="ID_1976080611" name="Cardápio" opensAsRoot="false"/>
+</bookmarks>
+<node TEXT="Cardápio digital" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1775052259245" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#eeeeeeff" zoom="0.82644624">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_grey_template.mm" followedTemplateLocation="template:/light_grey_template.mm" followedMapLastTime="1771883682000" show_tags="UNDER_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_364653791" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#676767" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_364653791" STARTINCLINATION="122.25 pt;0 pt;" ENDINCLINATION="119.25 pt;35.25 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#444444" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#3333ff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#3333ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1533685614" COLOR="#443333" BACKGROUND_COLOR="#dddddd" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc0033" TRANSPARENCY="255" DESTINATION="ID_1533685614"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#cc0033"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#222222" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#444444" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#676767" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#888888" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#222222" BACKGROUND_COLOR="#aaaaaa" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#222222" BACKGROUND_COLOR="#bbbbbb" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#222222" BACKGROUND_COLOR="#cccccc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#222222" BACKGROUND_COLOR="#dddddd" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#222222" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Home" ID="ID_1845149238" CREATED="1775052251481" MODIFIED="1775054646700" HGAP_QUANTITY="33.5 pt" VSHIFT_QUANTITY="-22.5 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Cardápio" POSITION="bottom_or_right" ID="ID_1976080611" CREATED="1775052477433" MODIFIED="1775052522848" HGAP_QUANTITY="56.75 pt" VSHIFT_QUANTITY="-33.75 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="" ID="ID_1388786527" CREATED="1775060857032" MODIFIED="1775060857032">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Mini-lanches" POSITION="bottom_or_right" ID="ID_1484693250" CREATED="1775052513998" MODIFIED="1775054543316" HGAP_QUANTITY="59.75 pt" VSHIFT_QUANTITY="16.5 pt"/>
+<node TEXT="Assados" ID="ID_407902819" CREATED="1775052513998" MODIFIED="1775054545907" HGAP_QUANTITY="60.5 pt" VSHIFT_QUANTITY="9 pt"/>
+<node TEXT="Salgateria" ID="ID_1663958943" CREATED="1775052513998" MODIFIED="1775054548589" HGAP_QUANTITY="60.5 pt" VSHIFT_QUANTITY="11.25 pt"/>
+<node TEXT="Acompanhamentos" ID="ID_316660777" CREATED="1775052513998" MODIFIED="1775054556642" HGAP_QUANTITY="60.5 pt" VSHIFT_QUANTITY="5.25 pt"/>
+<node TEXT="Panquecas" ID="ID_372331227" CREATED="1775052513998" MODIFIED="1775054571524" HGAP_QUANTITY="61.25 pt" VSHIFT_QUANTITY="2.25 pt"/>
+<node TEXT="Parmegianas" ID="ID_398942177" CREATED="1775052513998" MODIFIED="1775054573980" HGAP_QUANTITY="62.75 pt" VSHIFT_QUANTITY="1.5 pt"/>
+<node TEXT="Especiais" ID="ID_71140275" CREATED="1775052513998" MODIFIED="1775054577546" HGAP_QUANTITY="64.25 pt" VSHIFT_QUANTITY="-24.75 pt"/>
+<node TEXT="" ID="ID_775338849" CREATED="1775060857024" MODIFIED="1775060863510" VGAP_QUANTITY="3 pt">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Vendas/carrinho" ID="ID_825395212" CREATED="1775060857035" MODIFIED="1775060971574" HGAP_QUANTITY="57.5 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="aviso de permissões" ID="ID_263021103" CREATED="1775060941804" MODIFIED="1775061064213" HGAP_QUANTITY="27.5 pt" VSHIFT_QUANTITY="-1.5 pt">
+<node TEXT="" ID="ID_189084006" CREATED="1775061151647" MODIFIED="1775061151647">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="capturar localização" ID="ID_570936597" CREATED="1775061071139" MODIFIED="1775061080126"/>
+<node TEXT="capturar num telefone" ID="ID_1297521212" CREATED="1775061085923" MODIFIED="1775061130476"/>
+<node TEXT="" ID="ID_547474712" CREATED="1775061151644" MODIFIED="1775061151647">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="" ID="ID_493106346" CREATED="1775061195077" MODIFIED="1775061195077">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="exibe valor" ID="ID_1318332026" CREATED="1775061151649" MODIFIED="1775061167395"/>
+<node TEXT="" ID="ID_641819875" CREATED="1775061195075" MODIFIED="1775061195077">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="forma de pagamento" ID="ID_1195305567" CREATED="1775061195079" MODIFIED="1775061206870">
+<node TEXT="dinheiro" ID="ID_1190339040" CREATED="1775061215827" MODIFIED="1775061272051"/>
+<node TEXT="whatsapp/pix" ID="ID_195208922" CREATED="1775061232811" MODIFIED="1775061309579"/>
+<node TEXT="débito/crédito" ID="ID_575482026" CREATED="1775061248635" MODIFIED="1775061264475"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Contato" ID="ID_974004934" CREATED="1775052305525" MODIFIED="1775054898340" HGAP_QUANTITY="33.5 pt" VSHIFT_QUANTITY="2.25 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Redes sociais" ID="ID_471350660" CREATED="1775052513998" MODIFIED="1775054889697" HGAP_QUANTITY="35 pt" VSHIFT_QUANTITY="-8.25 pt"/>
+<node TEXT="Formulário de contato" ID="ID_1894217294" CREATED="1775052513998" MODIFIED="1775054885331" HGAP_QUANTITY="32 pt" VSHIFT_QUANTITY="13.5 pt"/>
+</node>
+<node TEXT="Sobre" ID="ID_394082670" CREATED="1775052345208" MODIFIED="1775054949466" HGAP_QUANTITY="32 pt" VSHIFT_QUANTITY="45.75 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Institucional" ID="ID_726236950" CREATED="1775052513998" MODIFIED="1775054929827" HGAP_QUANTITY="43.25 pt" VSHIFT_QUANTITY="-37.5 pt"/>
+<node TEXT="Divulgações" ID="ID_8878072" CREATED="1775052513998" MODIFIED="1775054949466" HGAP_QUANTITY="43.25 pt" VSHIFT_QUANTITY="32.25 pt"/>
+</node>
+</node>
+</map>
