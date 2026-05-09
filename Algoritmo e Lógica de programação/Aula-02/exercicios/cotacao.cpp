615edf68024325c6cf7a378e3364e105eb7fdcf5
mateustrozzi <mateustrozzi@outlook.com>
2026-05-09 12:20:31 -0300

    Atualizando o .gitignore

--- Algoritmo e Lógica de programação/Aula-02/exercicios/cotacao.cpp
+++ Algoritmo e Lógica de programação/Aula-02/exercicios/cotacao.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <iomanip>
+#include <stdio.h>
 
 using namespace std;
 
 int main(){
     float dolar, real, cotacao;
+    cout << fixed << setprecision(2);
+    printf("Olá mundo usando o printf\n");
     cout << "Qual é a cotação do dólar hoje?\n";
     cin >> dolar;
     cout << "Qunatos dólares você pretende cotar hoje?\n";
