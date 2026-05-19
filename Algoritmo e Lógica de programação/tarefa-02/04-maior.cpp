9f9c338bdb0bd722ff8a0c12912d8fa059656d21
mateustrozzi <mateustrozzi@outlook.com>
2026-05-19 19:49:54 -0300

    Alterações na estrutura do exercício 4 da tarefa 2

--- Algoritmo e Lógica de programação/tarefa-02/04-maior.cpp
+++ Algoritmo e Lógica de programação/tarefa-02/04-maior.cpp
@@ -10,70 +10,125 @@ int main() {
         std::setlocale(LC_ALL, ".UTF-8");
     #endif
 
-    limparTela();
+    //limparTela();
 
-    int n1, n2, n3;
-    center("============================================================");
+    int n1, n2, n3, maior, menor, quase;
+    //center("============================================================");
     center("QUAL É O MAIOR?");    
-    center("============================================================");
+    //center("============================================================");
     
     std::cout << "Entre com um número:\n";
     std::cin >> n1;
     std::cin.ignore();//limpa o buffer do teclado 
     std::cout << "Entre com outro número:\n";
     std::cin >> n2;
-    std::cin.ignore();//limpa o buffer do teclado 
+    std::cin.ignore();
     std::cout << "Entre com outro número:\n";
     std::cin >> n3;
-    std::cin.ignore();//limpa o buffer do teclado 
+    std::cin.ignore();
 
     std::stringstream titulo;
     std::stringstream mensagem;
+
+    //Tratamento de igualdades
     
     if(n1 == n2 && n1 == n3 && n2 == n3){
         titulo << "IGUAIS";
         mensagem << "O número " << n1 << ", " << n2 << " e " << n3 << " são iguais.";
-        if(n1 == n2){
-            titulo << "REPETIDOS";
-            if(n1 > n3){
-                mensagem << "O número " << n1 << " é igual ao " << n2 << ". E são maiores que " << n3;
-            }else{
-                mensagem << "O número " << n1 << " é igual ao " << n2 << ". E são menores que " << n3;
-            }
+
+        //limparTela();
+    
+        //center("============================================================");
+        center(titulo.str());    
+        //center("============================================================");
+        center(mensagem.str());
+        return 0;
+    }
+    if(n1 == n2){
+        if(n1 > n3){
+            titulo << "NEM TODOS SÃO DIFERENTES";
+            mensagem << "O número " << n1 << ", " << n2 << " são iguais e maiores que " << n3 << ".";
         }
-        else if(n2 == n3){
-            titulo << "REPETIDOS";
-            if(n1 > n3){
-                mensagem << "O número " << n2 << " é igual ao " << n2 << ". E são menores que " << n1;
-            }else{
-                mensagem << "O número " << n2 << " é igual ao " << n3 << ". E são maiores que " << n1;
-            }
+        else{
+            titulo << "NEM TODOS SÃO DIFERENTES";
+            mensagem << "O número " << n1 << ", " << n2 << " são iguais e menores que " << n3 << ".";
+        }
+        //limparTela();
+    
+        //center("============================================================");
+        center(titulo.str());    
+        //center("============================================================");
+        center(mensagem.str());
+        return 0;
+    }
+    if(n2 == n3){
+        if(n1 > n2){
+            titulo << "NEM TODOS SÃO DIFERENTES";
+            mensagem << "O número " << n2 << ", " << n3 << " são iguais e menores que " << n1 << ".";
+        }
+        else{
+            titulo << "NEM TODOS SÃO DIFERENTES";
+            mensagem << "O número " << n2 << ", " << n3 << " são iguais e menores que " << n1 << ".";
+        }
+        //limparTela();
+    
+        //center("============================================================");
+        center(titulo.str());    
+        //center("============================================================");
+        center(mensagem.str());
+        return 0;
+    }
+
+    //busca pelo maior
+
+    if(n1 > n2 && n1 > n3){
+        maior = n1;
+    }else if(n2 > n1 && n2 > n3){
+        maior = n2;
+    }else if(n3 > n1 && n3 > n2){
+        maior = n3;
+    }
+
+    // busca pelo menor
+
+    if(n1 < n2 && n1 < n3){
+        menor = n1;
+    }
+    else if(n2 < n1 && n2 < n3){
+        menor = n2;
+    }
+    else if(n3 < n1 && n3 < n2){
+        menor = n3;
+    }
+    //montando os titulos
+    if(maior == n1){
+        titulo << "O PRIMEIRO NÚMERO É MAIOR";
+        if(n3 == menor){
+            mensagem << "O número " << maior << "é maior que " << n2 << "que é maior que " << menor; 
+        }
+        else{
+            mensagem << "O número " << maior << "é maior que " << n3 << "que é maior que " << menor; 
         }
     }
-    else if(n1 > n2 && n1 > n3){
-        titulo << "O PRIMEIRO É MAIOR";
-        mensagem << "O número " << n1 << " é maior que " << n2 << " e " << n3;
-    }    
-    else if(n2 > n1 && n2 > n3){
-        titulo << "O SEGUNDO É MAIOR";
-        mensagem << "O número " << n2 << " é maior que " << n1 << " e " << n3;
-    }    
+    else if(maior == n2){
+        titulo << "O SEGUNDO NÚMERO É MAIOR";
+        if(n1 == menor){
+            mensagem << "O número " << maior << "é maior que " << n3 << "que é maior que " << menor; 
+        }
+    }
     else{
-        titulo << "O TERCEIRO É MAIOR";
-        mensagem << "O número " << n3 << " é maior que " << n1 << " e " << n2;
-    }
+        titulo << "O TERCEIRO NÚMERO É MAIOR";
+        mensagem << "O número " << maior << " é maior que " << n1 << " que é maior que " << menor;
+    }        
     
-    limparTela();
+    //limparTela();
     
-    center("============================================================");
+    //center("============================================================");
     center(titulo.str());    
-    center("============================================================");
-    center(mensagem.str());        
-    
+    //center("============================================================");
+    center(mensagem.str());            
 
-    std::cout << "\n\n\n" <<std::endl;
-    
-
+    std::cout << "\n\n\n" <<std::endl;   
 
     return 0;
 }
