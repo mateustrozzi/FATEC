593c50d596117d045b4a02272aeaeb645a4e7746
mateustrozzi <mateustrozzi@outlook.com>
2026-05-19 22:58:56 -0300

    Alterando a estrutura do exercício 04 da tarefa 2 - Lógica e Algoritmos

--- Algoritmo e Lógica de programação/tarefa-02/04-maior.cpp
+++ Algoritmo e Lógica de programação/tarefa-02/04-maior.cpp
@@ -13,9 +13,7 @@ int main() {
     //limparTela();
 
     int n1, n2, n3, maior, menor, quase;
-    //center("============================================================");
     center("QUAL É O MAIOR?");    
-    //center("============================================================");
     
     std::cout << "Entre com um número:\n";
     std::cin >> n1;
@@ -30,105 +28,66 @@ int main() {
     std::stringstream titulo;
     std::stringstream mensagem;
 
-    //Tratamento de igualdades
-    
-    if(n1 == n2 && n1 == n3 && n2 == n3){
+    // Tratamento de igualdades (mantive a sua lógica que já funciona bem)
+    if(n1 == n2 && n1 == n3){
         titulo << "IGUAIS";
-        mensagem << "O número " << n1 << ", " << n2 << " e " << n3 << " são iguais.";
-
-        //limparTela();
-    
-        //center("============================================================");
+        mensagem << "Os números " << n1 << ", " << n2 << " e " << n3 << " são iguais.";
         center(titulo.str());    
-        //center("============================================================");
         center(mensagem.str());
         return 0;
     }
     if(n1 == n2){
-        if(n1 > n3){
-            titulo << "NEM TODOS SÃO DIFERENTES";
-            mensagem << "O número " << n1 << ", " << n2 << " são iguais e maiores que " << n3 << ".";
-        }
-        else{
-            titulo << "NEM TODOS SÃO DIFERENTES";
-            mensagem << "O número " << n1 << ", " << n2 << " são iguais e menores que " << n3 << ".";
-        }
-        //limparTela();
-    
-        //center("============================================================");
+        titulo << "NEM TODOS SÃO DIFERENTES";
+        if(n1 > n3) mensagem << "Os números " << n1 << " e " << n2 << " são iguais e maiores que " << n3 << ".";
+        else        mensagem << "Os números " << n1 << " e " << n2 << " são iguais e menores que " << n3 << ".";
         center(titulo.str());    
-        //center("============================================================");
         center(mensagem.str());
         return 0;
     }
     if(n2 == n3){
-        if(n1 > n2){
-            titulo << "NEM TODOS SÃO DIFERENTES";
-            mensagem << "O número " << n2 << ", " << n3 << " são iguais e menores que " << n1 << ".";
-        }
-        else{
-            titulo << "NEM TODOS SÃO DIFERENTES";
-            mensagem << "O número " << n2 << ", " << n3 << " são iguais e menores que " << n1 << ".";
-        }
-        //limparTela();
-    
-        //center("============================================================");
+        titulo << "NEM TODOS SÃO DIFERENTES";
+        if(n1 > n2) mensagem << "O número " << n1 << " é maior que os iguais " << n2 << " e " << n3 << ".";
+        else        mensagem << "O número " << n1 << " é menor que os iguais " << n2 << " e " << n3 << ".";
         center(titulo.str());    
-        //center("============================================================");
+        center(mensagem.str());
+        return 0;
+    }
+    if(n1 == n3){
+        titulo << "NEM TODOS SÃO DIFERENTES";
+        if(n2 > n1) mensagem << "O número " << n2 << " é maior que os iguais " << n1 << " e " << n3 << ".";
+        else        mensagem << "O número " << n2 << " é menor que os iguais " << n1 << " e " << n3 << ".";
+        center(titulo.str());    
         center(mensagem.str());
         return 0;
     }
 
-    //busca pelo maior
+    // --- NOVA LÓGICA DE ORDENAÇÃO (Para quando todos são diferentes) ---
 
-    if(n1 > n2 && n1 > n3){
-        maior = n1;
-    }else if(n2 > n1 && n2 > n3){
-        maior = n2;
-    }else if(n3 > n1 && n3 > n2){
-        maior = n3;
-    }
+    // 1. Descobre o Maior
+    if (n1 > n2 && n1 > n3)      maior = n1;
+    else if (n2 > n1 && n2 > n3) maior = n2;
+    else                         maior = n3;
 
-    // busca pelo menor
+    // 2. Descobre o Menor
+    if (n1 < n2 && n1 < n3)      menor = n1;
+    else if (n2 < n1 && n2 < n3) menor = n2;
+    else                         menor = n3;
 
-    if(n1 < n2 && n1 < n3){
-        menor = n1;
-    }
-    else if(n2 < n1 && n2 < n3){
-        menor = n2;
-    }
-    else if(n3 < n1 && n3 < n2){
-        menor = n3;
-    }
-    //montando os titulos
-    if(maior == n1){
-        titulo << "O PRIMEIRO NÚMERO É MAIOR";
-        if(n3 == menor){
-            mensagem << "O número " << maior << "é maior que " << n2 << "que é maior que " << menor; 
-        }
-        else{
-            mensagem << "O número " << maior << "é maior que " << n3 << "que é maior que " << menor; 
-        }
-    }
-    else if(maior == n2){
-        titulo << "O SEGUNDO NÚMERO É MAIOR";
-        if(n1 == menor){
-            mensagem << "O número " << maior << "é maior que " << n3 << "que é maior que " << menor; 
-        }
-    }
-    else{
-        titulo << "O TERCEIRO NÚMERO É MAIOR";
-        mensagem << "O número " << maior << " é maior que " << n1 << " que é maior que " << menor;
-    }        
+    // 3. O do meio (quase) é a sobra: a soma de todos menos o maior e o menor
+    quase = (n1 + n2 + n3) - maior - menor;
+
+    // Definindo o título dinamicamente baseado em quem é o maior
+    if (maior == n1)      titulo << "O PRIMEIRO NÚMERO É MAIOR";
+    else if (maior == n2) titulo << "O SEGUNDO NÚMERO É MAIOR";
+    else                  titulo << "O TERCEIRO NÚMERO É MAIOR";
+
+    // Agora a mensagem fica universal e simples, sem ifs complexos!
+    mensagem << "O número " << maior << " é maior que " << quase << " que é maior que " << menor << ".";
     
-    //limparTela();
-    
-    //center("============================================================");
+    // Exibição dos resultados
     center(titulo.str());    
-    //center("============================================================");
     center(mensagem.str());            
 
-    std::cout << "\n\n\n" <<std::endl;   
-
+    std::cout << "\n\n\n" << std::endl;   
     return 0;
 }
